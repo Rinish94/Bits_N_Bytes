641895e024b2baa4f0796f90c6847226bc0e075e
Ashutosh Pandey <rojha2030@gmail.com>
2021-08-07 17:30:34 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import { Routes } from './Routes/Routes';
+import { Routes } from './Routes/Routes';
 import Chat from './Components/ChatApp/Chat'
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes/> */}
-      <Chat/>
+      <Routes/>
+      {/* <Chat/> */}
     </div>
   );
 }
