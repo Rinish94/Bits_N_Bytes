b06ee4cbbe47f1c70f857033579656a74043e800
PRASADBIRARI <prasadpbirari7@gmail.com>
2021-08-07 12:58:20 +0530

    typewriter effect done

--- frontend/src/Components/Home/DUK.jsx
+++ frontend/src/Components/Home/DUK.jsx
@@ -57,7 +57,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import styles from "./DUK.module.css";
+import styles from "./Duk.module.css";
 import ListItem from '@material-ui/core/ListItem';
 
 const useStyles = makeStyles((theme) => ({
